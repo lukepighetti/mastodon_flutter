4312af303802b89d64a964619f4d598f03f32fe3
Luke Pighetti <lukepighetti@gmail.com>
2019-03-02 15:04:26 -0500

    add _MediaGrid that meets specifications

--- example/lib/timeline/media.dart
+++ example/lib/timeline/media.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:mastodon/mastodon.dart';
 
 class Media extends StatelessWidget {
@@ -11,8 +12,6 @@ class Media extends StatelessWidget {
       .take(4)
       .toList();
 
-  int get imagesCount => images.length;
-
   bool get hasImages => images.isNotEmpty;
 
   @override
@@ -24,29 +23,109 @@ class Media extends StatelessWidget {
         padding: const EdgeInsets.symmetric(vertical: 8.0),
         child: ClipRRect(
           borderRadius: BorderRadius.circular(8),
-          child: LayoutBuilder(
-            builder: (_, box) {
-              final width = box.maxWidth;
-
-              return Container(
-                constraints: BoxConstraints(maxHeight: width),
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: images
-                      .map((i) => Flexible(
-                            child: Image.network(
-                              i.previewUrl.toString(),
-                              fit: BoxFit.cover,
-                            ),
-                          ))
-                      .toList(),
-                ),
-              );
-            },
+          child: _MediaGrid(
+            children: images
+                .map((i) => Image.network(
+                      i.previewUrl.toString(),
+                      fit: BoxFit.cover,
+                    ))
+                .toList(),
           ),
         ),
       );
+  }
+}
+
+/// Takes 1-4 widgets and lays them out in a social-media style grid.
+/// [aspectRatio] and [gap] can both be overriden.
+///
+/// A social-media style grid is common in the industry. It has a [gap],
+/// maintains an [aspectRatio], and lays out up to four images in the following
+/// order:
+///
+/// ```
+/// One   Two   Three  Four
+/// 1 1   1 2   1 2    1 2
+/// 1 1   1 2   1 3    3 4
+///```
+///
+/// Note: Every widget is wrapped in an Expanded widget.
+class _MediaGrid extends StatelessWidget {
+  final List<Widget> children;
+  final double aspectRatio;
+  final double gap;
+
+  _MediaGrid({
+    @required List<Widget> children,
+    this.aspectRatio = 1.6,
+    this.gap = 4.0,
+  })  : this.children = children.map((c) => Expanded(child: c)).toList(),
+        assert(children.length >= 1),
+        assert(children.length <= 4);
+
+  int get count => children.length;
+
+  List<List<Widget>> get layout {
+    switch (count) {
+      case 1:
+        return [
+          [children.first],
+          [],
+        ];
+      case 2:
+        return [
+          [children.first],
+          [children[1]],
+        ];
+      case 3:
+        return [
+          [children.first],
+          [children[1], SizedBox(height: gap), children[2]],
+        ];
+      case 4:
+        return [
+          [children.first, SizedBox(height: gap), children[2]],
+          [children[1], SizedBox(height: gap), children[3]],
+        ];
+
+      default:
+        return null;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AspectRatio(
+      aspectRatio: aspectRatio,
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          /// Left column
+          Flexible(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: layout.first,
+            ),
+          ),
+
+          /// Gap
+          count >= 2
+              ? SizedBox(
+                  width: gap,
+                )
+              : Container(),
+
+          /// Right column
+          layout.last.isEmpty
+              ? Container()
+              : Flexible(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: layout.last,
+                  ),
+                ),
+        ],
+      ),
+    );
   }
 }
