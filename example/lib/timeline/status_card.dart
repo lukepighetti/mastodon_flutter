6944de1ac6a91b5a100f187446038169f38389ee
Luke Pighetti <lukepighetti@gmail.com>
2019-03-02 09:18:16 -0500

    factor out Media

--- example/lib/timeline/status_card.dart
+++ example/lib/timeline/status_card.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:mastodon/mastodon.dart' hide Card;
 
 import 'package:flutter_html/flutter_html.dart';
 import 'package:timeago/timeago.dart' as timeago;
-import 'package:mastodon/mastodon.dart' hide Card;
 import 'package:feather_icons_flutter/feather_icons_flutter.dart';
-
 import 'package:url_launcher/url_launcher.dart';
+
+import 'media.dart';
 
 class StatusCard extends StatelessWidget {
   final Status status;
@@ -96,7 +97,7 @@ class _PosterSection extends StatelessWidget {
                   onLinkTap: (url) => launch(url),
                   blockSpacing: 4,
                 ),
-                _Media(
+                Media(
                   status: status,
                 ),
                 _Actions(
@@ -108,56 +109,6 @@ class _PosterSection extends StatelessWidget {
         ),
       ],
     );
-  }
-}
-
-class _Media extends StatelessWidget {
-  final Status status;
-
-  _Media({@required this.status});
-
-  List<Attachment> get images => status.mediaAttachments
-      .where((a) => a.type == AttachmentType.image)
-      .take(4)
-      .toList();
-
-  int get imagesCount => images.length;
-
-  bool get hasImages => images.isNotEmpty;
-
-  @override
-  Widget build(BuildContext context) {
-    if (!hasImages)
-      return Container();
-    else
-      return Padding(
-        padding: const EdgeInsets.symmetric(vertical: 8.0),
-        child: ClipRRect(
-          borderRadius: BorderRadius.circular(8),
-          child: LayoutBuilder(
-            builder: (_, box) {
-              final width = box.maxWidth;
-
-              return Container(
-                constraints: BoxConstraints(maxHeight: width),
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: images
-                      .map((i) => Flexible(
-                            child: Image.network(
-                              i.previewUrl.toString(),
-                              fit: BoxFit.cover,
-                            ),
-                          ))
-                      .toList(),
-                ),
-              );
-            },
-          ),
-        ),
-      );
   }
 }
 
