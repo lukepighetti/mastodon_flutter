9f752410bd4f8c41980746c6aecb91e5ac98a47d
Luke Pighetti <lukepighetti@gmail.com>
2019-03-01 23:05:58 -0500

    show first image

--- example/lib/timeline/status_card.dart
+++ example/lib/timeline/status_card.dart
@@ -96,6 +96,9 @@ class _PosterSection extends StatelessWidget {
                   onLinkTap: (url) => launch(url),
                   blockSpacing: 4,
                 ),
+                _Media(
+                  status: status,
+                ),
                 _Actions(
                   status: status,
                 ),
@@ -105,6 +108,56 @@ class _PosterSection extends StatelessWidget {
         ),
       ],
     );
+  }
+}
+
+class _Media extends StatelessWidget {
+  final Status status;
+
+  _Media({@required this.status});
+
+  List<Attachment> get images => status.mediaAttachments
+      .where((a) => a.type == AttachmentType.image)
+      .take(4)
+      .toList();
+
+  int get imagesCount => images.length;
+
+  bool get hasImages => images.isNotEmpty;
+
+  @override
+  Widget build(BuildContext context) {
+    if (!hasImages)
+      return Container();
+    else
+      return Padding(
+        padding: const EdgeInsets.symmetric(vertical: 8.0),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(8),
+          child: LayoutBuilder(
+            builder: (_, box) {
+              final width = box.maxWidth;
+
+              return Container(
+                constraints: BoxConstraints(maxHeight: width),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: images
+                      .map((i) => Flexible(
+                            child: Image.network(
+                              i.previewUrl.toString(),
+                              fit: BoxFit.cover,
+                            ),
+                          ))
+                      .toList(),
+                ),
+              );
+            },
+          ),
+        ),
+      );
   }
 }
 
