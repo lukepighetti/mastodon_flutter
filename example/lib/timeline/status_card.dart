6b5fd9e623318ece6b1c15b1165314aed92bd6cf
Luke Pighetti <lukepighetti@gmail.com>
2019-03-06 10:00:20 -0500

    move Content to mastodon_flutter exports

--- example/lib/timeline/status_card.dart
+++ example/lib/timeline/status_card.dart
@@ -1,6 +1,6 @@
-import 'package:example/widgets/content.dart';
 import 'package:flutter/material.dart';
 import 'package:mastodon/mastodon.dart' hide Card;
+import 'package:mastodon_flutter/mastodon_flutter.dart';
 
 import 'package:timeago/timeago.dart' as timeago;
 import 'package:feather_icons_flutter/feather_icons_flutter.dart';
--- lib/mastodon_flutter.dart
+++ lib/mastodon_flutter.dart
@@ -3,3 +3,4 @@ library mastodon_flutter;
 export "src/auth_builder.dart";
 export "src/auth_storage.dart";
 export "src/emoji_text_span.dart";
+export "src/content.dart";
