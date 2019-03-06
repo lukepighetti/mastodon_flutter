125a255e79ad282e5b958036ad47e5637294c178
Luke Pighetti <lukepighetti@gmail.com>
2019-03-06 09:58:22 -0500

    add Content widget, remove flutter_html dependency

--- example/lib/widgets/content.dart
+++ example/lib/widgets/content.dart
@@ -1,46 +1,108 @@
-import 'package:flutter/gestures.dart';
-import 'package:flutter/material.dart' hide Element;
+import 'package:flutter/material.dart' hide Element, Text;
 import 'package:mastodon/mastodon.dart';
+import 'package:mastodon_flutter/mastodon_flutter.dart';
 
-import 'package:url_launcher/url_launcher.dart';
-import 'package:flutter_html/flutter_html.dart';
-
-import 'package:html/dom.dart' hide Text;
+import 'package:html/dom.dart';
 import 'package:html/parser.dart';
 
+/// A dead-simple HTML parser that handles 99% of Mastodon
+/// content.
+///
+/// ActivityPub allows any HTML tags in the content field,
+/// but Mastodon only uses a few tags with classnames. Instead
+/// of using a more robust HTML parsing widget, we pick out the
+/// common cases and build them perfectly.
+///
+/// TODO: add mention/hashtag/link tap gestures
+/// TODO: use app theme?
 class Content extends StatelessWidget {
   final Document html;
+  final Status status;
 
-  Content({@required Status status}) : html = parse(status.content);
+  Content({@required Status status})
+      : html = parse(status.content),
+        status = status;
 
+  Element get body => html.body;
+  String get text => html.body.text;
   String get content => html.body.innerHtml;
+
+  TextStyle get textStyle => TextStyle(
+        fontSize: 16,
+        height: 1.1,
+        color: Colors.black,
+      );
+
+  TextStyle get linkStyle => textStyle.copyWith(
+        color: Colors.blue,
+      );
+
+  List<TextSpan> parseNode(Node message) {
+    final spans = <TextSpan>[];
+
+    for (final node in message.nodes) {
+      if (node is Text) {
+        spans.add(EmojiTextSpan(
+          text: node.text,
+        ));
+      } else if (node is Element) {
+        switch (node.localName) {
+          case "a":
+            spans.add(TextSpan(
+              children: parseNode(node),
+              style: linkStyle,
+            ));
+            break;
+
+          case "p":
+            spans.addAll(parseNode(node));
+            spans.add(TextSpan(text: "\n\n"));
+            break;
+
+          case "br":
+            spans.add(TextSpan(text: "\n"));
+            break;
+
+          case "span":
+            switch (node.className) {
+              case "invisible":
+                break;
+              case "ellipsis":
+                spans.add(EmojiTextSpan(text: node.text + "..."));
+                break;
+              default:
+                spans.addAll(parseNode(node));
+            }
+            break;
+
+          default:
+            spans.addAll(parseNode(node));
+        }
+      }
+    }
+
+    final allWhitespace = RegExp(r"^\s*$", multiLine: false);
+
+    /// Remove all the trailing whitespace
+    final filtered = spans.reversed
+        .skipWhile(
+          (span) => allWhitespace.hasMatch(span?.toPlainText()),
+        )
+        .toList()
+        .reversed
+        .toList();
+
+    return filtered;
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Html(
-      data: content,
-      onLinkTap: (url) => launch(url),
-      blockSpacing: 8,
-      defaultTextStyle: TextStyle(fontSize: 16),
-      renderNewlines: true,
-      customRender: (node, __) {
-        if (node is Element) {
-          switch (node.className) {
-            case "invisible":
-              return Container();
-            case "ellipsis":
-              return null;
-            case "mention":
-              return null;
-            case "hashtag":
-              return null;
-          }
-        }
-      },
-      linkStyle: TextStyle(
-        fontSize: 16,
-        fontWeight: FontWeight.w500,
-        color: Theme.of(context).primaryColor,
+    final spans = parseNode(html.body);
+
+    return RichText(
+      text: TextSpan(
+        style: textStyle,
+        children: spans,
       ),
     );
   }
